cea459dc265d84b8b8aaa0abbaa70e676ab57e84
Oliver Beerworth <129229882+Sp0ngeman@users.noreply.github.com>
2023-08-24 01:47:36 +0000

    test

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,53 +9,68 @@ import React from "react";
 import arrowSvg from "../images/down-arrow.svg";
 import PropTypes from "prop-types";
 
-/**
- * Home background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that
- * represents what you see in that image.
- *
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
 import image from "../images/Jack-Field.png";
 
-const imageAltText = "Jack Fitch standing in a rugby stadium.";
+const Home = () => {
+  const handleArrowClick = () => {
+    // Scroll to the 'portfolio' section
+    const portfolioSection = document.getElementById("portfolio");
+    if (portfolioSection) {
+      portfolioSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  /**
+   * Home background image
+   *
+   * Below is a sample image. Upload the image of your choice into the "images"
+   * directory and import here for use. Then, set imageAltText to string that
+   * represents what you see in that image.
+   *
+   *
+   * Need an image? Check out https://unsplash.com to download a photo you
+   * freely use on your site.
+   */
 
-const Home = ({ name, title }) => {
-  return (
-    <section id="home" className="min-height">
-      <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
-        <h1>{name}</h1>
-        <h2>{title}</h2>
-      </div>
-      <div
-        style={{
-          position: "fixed",
-          bottom: "3rem",
-          left: "50%",
-          transform: "translateX(-50%)",
-          display: "flex",
-          alignItems: "center",
-        }}
+  const imageAltText = "Jack Fitch standing in a rugby stadium.";
+
+  const Home = ({ name, title }) => {
+    return (
+      <section
+        id="home"
+        style={{ minWidth: "100vw", minHeight: "100vh", display: "flex", flexDirection: "column" }}
+        className="min-height"
       >
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
-      </div>
-    </section>
-  );
-};
+        <img className="background" src={image} alt="" />
+        <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+          <h1>{name}</h1>
+          <h2>{title}</h2>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            marginTop: "auto",
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginBottom: "3rem",
+          }}
+        >
+          onClick={handleArrowClick}
+          <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        </div>
+      </section>
+    );
+  };
 
-Home.defaultProps = {
-  name: "",
-  title: "",
-};
+  Home.defaultProps = {
+    name: "",
+    title: "",
+  };
 
-Home.propTypes = {
-  name: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
+  Home.propTypes = {
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  };
 };
 
 export default Home;
