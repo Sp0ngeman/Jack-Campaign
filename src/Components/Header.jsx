d38494dc92e238412bc95cfe93eb5a182e05dd02
Oliver Beerworth <129229882+Sp0ngeman@users.noreply.github.com>
2023-08-24 05:14:07 +0000

    test

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,10 @@
+/**
+ * Header component
+ *
+ * Top navigation bar for your site. Set to remain visible as the
+ * user scrolls so that they can constantly reach any part of your page.
+ */
 import React from "react";
-import { Link } from "react-scroll";
 
 const Header = () => {
   return (
@@ -16,18 +21,10 @@ const Header = () => {
         zIndex: 10,
       }}
     >
-      <Link to="home" smooth={true} duration={500}>
-        Home
-      </Link>
-      <Link to="about" smooth={true} duration={500}>
-        About
-      </Link>
-      <Link to="portfolio" smooth={true} duration={500}>
-        Portfolio
-      </Link>
-      <Link to="footer" smooth={true} duration={500}>
-        Contact
-      </Link>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#portfolio">Portfolio</a>
+      <a href="#footer">Contact</a>
     </div>
   );
 };
