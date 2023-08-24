b47ef51f9a05d4a874875d648de97ec221c8968f
Oliver Beerworth <129229882+Sp0ngeman@users.noreply.github.com>
2023-08-24 00:41:27 +0000

    test

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,54 +1,38 @@
-/**
- * About component
- *
- * Space for you to describe more about yourself.
- */
-
-import React from "react";
+import React, { useState } from "react";
 import arrowSvg from "../images/down-arrow.svg";
 
-/**
- * About background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that
- * represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a image you
- * freely use on your site.
- */
 import image from "../images/motion-background.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
-/**
- * Sort description that expands on your title on the Home component.
- */
-const description =
-  'I am a passionate sport advocate and Rugby enjoyer. I love the "Art" of food and do as much as I can to spread that sentiment to others.';
-
-/**
- * List of some of skills or technologies you work on, are learning,
- * passionate about, or enjoy,
- */
-const skillsList = [
-  "Rugby Player",
-  "Sports Enthusiast",
-  "Charity Worker",
-  "Food connoisseur",
-  "Leader",
-  "Chisolm Vice Captain Candidate",
-];
-
-/**
- * Use this to give more information about what you are passionate about,
- * how you best work, or even a quote. This will help someone learn more
- * about you on a professional level.
- */
-const detailOrQuote =
-  "I love to work with people and help them achieve their goals. I am a firm believer in the power of positivity and the impact it can have on others.";
-
 const About = () => {
+  const [isArrowClicked, setIsArrowClicked] = useState(false);
+
+  const handleArrowClick = () => {
+    setIsArrowClicked(true);
+
+    // Scroll to the 'portfolio' section
+    const portfolioSection = document.getElementById("portfolio");
+    if (portfolioSection) {
+      portfolioSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const description =
+    'I am a passionate sport advocate and Rugby enjoyer. I love the "Art" of food and do as much as I can to spread that sentiment to others.';
+
+  const skillsList = [
+    "Rugby Player",
+    "Sports Enthusiast",
+    "Charity Worker",
+    "Food connoisseur",
+    "Leader",
+    "Chisolm Vice Captain Candidate",
+  ];
+
+  const detailOrQuote =
+    "I love to work with people and help them achieve their goals. I am a firm believer in the power of positivity and the impact it can have on others.";
+
   return (
     <section className="padding" id="about">
       <img className="background" src={image} alt={imageAltText} />
@@ -71,6 +55,8 @@ const About = () => {
             fontSize: "1.25rem",
             margin: "2rem 3rem",
             gap: "3rem",
+            listStyle: "none",
+            paddingInlineStart: 0,
           }}
         >
           {skillsList.map((skill) => (
@@ -78,10 +64,36 @@ const About = () => {
           ))}
         </ul>
         <hr />
-        <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
-      </div>
-      <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        <p style={{ padding: "1rem 3rem 0", textAlign: "center" }}>{detailOrQuote}</p>
+        <div
+          style={{
+            position: "absolute",
+            bottom: "3rem",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          <button
+            style={{
+              border: "none",
+              background: "transparent",
+              cursor: "pointer",
+              padding: "0",
+              display: "block",
+            }}
+            onClick={handleArrowClick}
+          >
+            <img
+              src={arrowSvg}
+              style={{
+                height: "3rem",
+                width: "3rem",
+                filter: isArrowClicked ? "invert(100%)" : "none",
+              }}
+              alt="Arrow"
+            />
+          </button>
+        </div>
       </div>
     </section>
   );
